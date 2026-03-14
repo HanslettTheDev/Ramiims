59c855003e5c682266859001189d0c12642e5312
Venom <hanslettthedev@gmail.com>
2026-03-14 10:05:27 +0100

    feat: reconciled the frontend files to use the latest version of view

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from "./router"
+import "./assets/main.css";
 
-const app = createApp(App);
+import { createApp } from "vue";
+import App from "./App.vue";
 
-app.use(router);
-
-app.mount("#app");
+createApp(App).mount("#app");
